a4ce91760693071468aa6f9831788f8a5b7cf645
100462832 <100462832@alumnos.uc3m.es>
2026-04-19 16:32:11 +0200

    Correccion SQLi companies_admin.py

--- routes/companies_admin.py
+++ routes/companies_admin.py
@@ -19,7 +19,8 @@ def admin_add_company():
         company_name = request.form['company_name']
         owner = request.form['owner']
         conn = get_data_connection()
-        conn.execute("INSERT INTO companies (name, owner) VALUES ('"+ company_name+"', '"+owner+"')")
+        # Correccion 1: Consulta parametrizada
+        conn.execute("INSERT INTO companies (name, owner) VALUES (?, ?)", (company_name, owner))
         conn.commit()
         conn.close()
         flash("Company created successfully.", "success")
@@ -32,8 +33,9 @@ def delete_company():
         return render_template('errors/403.html'), 403
     company = request.form['company']
     conn = get_data_connection()
-    conn.execute("DELETE FROM companies WHERE id = "+ company)
-    conn.execute("DELETE FROM comments WHERE company_id = " + company)
+    # Correccion 2: Consultas parametrizadas
+    conn.execute("DELETE FROM companies WHERE id = ?", (company,))
+    conn.execute("DELETE FROM comments WHERE company_id = ?", (company,))
     conn.commit()
     conn.close()
     flash("Company deleted.", "warning")
